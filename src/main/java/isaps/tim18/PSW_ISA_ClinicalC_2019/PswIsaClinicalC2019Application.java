0d16ba6c43ca68d9b980b769fc2fb45657625748
ivanabrkic <ivanabrkic192@gmail.com>
2019-11-07 16:34:42 +0100

    Initialized project

---
+++ src/main/java/isaps/tim18/PSW_ISA_ClinicalC_2019/PswIsaClinicalC2019Application.java
@@ -0,0 +1,13 @@
+package isaps.tim18.PSW_ISA_ClinicalC_2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PswIsaClinicalC2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PswIsaClinicalC2019Application.class, args);
+	}
+
+}
---
+++ src/test/java/isaps/tim18/PSW_ISA_ClinicalC_2019/PswIsaClinicalC2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package isaps.tim18.PSW_ISA_ClinicalC_2019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PswIsaClinicalC2019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
