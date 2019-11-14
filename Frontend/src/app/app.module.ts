1ad113252133a95f329e0f64ae08d4807c3eb451
ivanabrkic <ivanabrkic192@gmail.com>
2019-11-14 22:57:03 +0100

    Profil administratora klinickog centra #2_8.2 frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,17 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AdministratorKlinikeComponent} from './modules/objects/administratorklinike/administrator_klinike.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {SidebarComponent} from './modules/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AdministratorKlinikeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    AngularFontAwesomeModule,
+    RouterModule.forRoot([{ path: 'administratorklinike', component: AdministratorKlinikeComponent},
+      { path: 'welcome', component: HomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', component: NotFoundComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/modules/objects/administratorklinike/administrator_klinike.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './administrator_klinike.component.html',
+  styleUrls: ['./administrator_klinike.component.css']
+})
+export class AdministratorKlinikeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
