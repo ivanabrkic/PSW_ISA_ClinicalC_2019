bf72d31ef86d4426816a19922f3769a8a9ed3144
huehue12 <lazarevict97@gmail.com>
2019-11-21 21:07:14 +0100

    izmena modela

--- Backend/src/main/java/isaps/tim18/PSW_ISA_ClinicalC_2019/repository/KorisnikRepository.java
+++ Backend/src/main/java/isaps/tim18/PSW_ISA_ClinicalC_2019/repository/KorisnikRepository.java
@@ -1,8 +1,16 @@
 package isaps.tim18.PSW_ISA_ClinicalC_2019.repository;
 
 import isaps.tim18.PSW_ISA_ClinicalC_2019.model.Korisnik;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
+
+    Page<Korisnik> findAll(Pageable pageable);
+
+    List<Korisnik> findAllByKorisnickoIme(String kime);
 
 }
