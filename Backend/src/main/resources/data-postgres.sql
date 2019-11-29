022425fada9f9f03f6bb8cd8281e7759b5a46ee3
ivanabrkic <ivanabrkic192@gmail.com>
2019-11-29 04:09:08 +0100

    Izmena podataka za ubacivanje u tabele

--- Backend/src/main/resources/data-postgres.sql
+++ Backend/src/main/resources/data-postgres.sql
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana) values (1, 'Backa Palanka', 'Srbija', 'Save Kovacevica 47', 123456789, True, 'ivanabrkic192@gmail.com', 'Ivana', '0652754579', 'ika', '123', 'Brkic', 65);
-insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana) values (2, 'Indjija', 'Srbija', 'Glavna 38', 987654321, True,'vladimirpopovic@gmail.com', 'Vladimir', '0652754579', 'vlada', '1234', 'Popovic', 23);
-insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana) values (3, 'Sabac', 'Srbija', 'Mladena Djuricica 11', 123123123, True, 'tamaralazarevic@gmail.com', 'Tamara', '0652754579', 'tesla', '1235', 'Lazarevic', 11);
-
-
-insert into administrator_klinike (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime) values (4, 'Novi Sad', 'Srbija', 'Mise Dimitrijevica 51', 123321123, True, 'anamarija@gmail.com', 'Ana-Marija', '0652754579', 'anacrtica', '123', 'Buhmiler');
-insert into administrator_klinike (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime) values (5, 'Nova Pazova', 'Srbija', 'Vojvode Misica 30', 654123654, True,'nikolamilosevic@gmail.com', 'Nikola', '0652754579', 'vanelee', '1234', 'Milosevic');
-
-insert into administrator_klinickog_centra (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime) values (1, 'Backa Palanka', 'Srbija', 'Save Kovacevica 47', 123456789, True, 'ivanabrkic192@gmail.com', 'Ivana', '0652754579', 'ika', '123', 'Brkic');
-
-insert into pacijent (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime) values (6, 'Beocin', 'Srbija', 'Zlatiborska 40', 876656654, True,'milanlux@gmail.com', 'Milan', '0652754579', 'lux', '1234', 'Lukovic');
-insert into pacijent (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime) values (7, 'Brazilija', 'Srbija', 'Tarska 40', 342378491, True, 'milanlukic@gmail.com', 'Milance', '0652754579', 'mix', '1235', 'Lukic');
-
-insert into medicinska_sestra (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana) values (8, 'Backa Palanka', 'Srbija', 'Save Kovacevica 50', 645238967, True, 'ivanabrkic192@gmail.com', 'Ivanica', '0652754579', 'maxiixa', '123', 'Maksimovica', 65);
-insert into medicinska_sestra (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana) values (9, 'Indjija', 'Srbija', 'Glavna 38', 645987234, True,'vladimirpopovic@gmail.com', 'Vladimirko', '0652754579', 'popvlada', '1234', 'Popov', 23);
-=======
 insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana, tip_korisnika) values (1, 'Backa Palanka', 'Srbija', 'Save Kovacevica 47', 123456789, True, 'ivanabrkic192@gmail.com', 'Ivana', '0652754579', 'ika', '12345678', 'Brkic', 65, 'Lekar');
 insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana, tip_korisnika) values (2, 'Indjija', 'Srbija', 'Glavna 38', 987654321, True,'vladimirpopovic@gmail.com', 'Vladimir', '0652754579', 'vlada', '12345678', 'Popovic', 23, 'Lekar');
 insert into lekar (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana, tip_korisnika) values (3, 'Sabac', 'Srbija', 'Mladena Djuricica 11', 123123123, True, 'tamaralazarevic@gmail.com', 'Tamara', '0652754579', 'tesla', '12345678', 'Lazarevic', 11, 'Lekar');
@@ -30,4 +13,3 @@ insert into pacijent (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, im
 
 insert into medicinska_sestra (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana, tip_korisnika) values (8, 'Backa Palanka', 'Srbija', 'Save Kovacevica 50', 645238967, True, 'ivanabrkic192@gmail.com', 'Ivanica', '0652754579', 'maxiixa', '12345678', 'Maksimovica', 65, 'Medicinska sestra');
 insert into medicinska_sestra (id, grad, drzava, adresa, jbo, aktivnost_naloga, email, ime, kontakt_telefon, kor_ime, lozinka, prezime, br_slobodnih_dana, tip_korisnika) values (9, 'Indjija', 'Srbija', 'Glavna 38', 645987234, True,'vladimirpopovic@gmail.com', 'Vladimirko', '0652754579', 'popvlada', '12345678', 'Popov', 23, 'Medicinska sestra');
->>>>>>> b828c1f87692579a75adc5b13ea6216189717ebd
