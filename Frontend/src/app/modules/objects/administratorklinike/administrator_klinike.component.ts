6b5683baa6118749f91c5dc852a9e1a092a6615c
ivanabrkic <ivanabrkic192@gmail.com>
2019-11-29 03:57:30 +0100

    Dodata polja u izmeni na frontendu i validacija!

--- Frontend/src/app/modules/objects/administratorklinike/administrator_klinike.component.ts
+++ Frontend/src/app/modules/objects/administratorklinike/administrator_klinike.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   templateUrl: './administrator_klinike.component.html',
@@ -6,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministratorKlinikeComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  adminForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.adminForm = this.formBuilder.group({
+      adresa: ['', Validators.required],
+      kontaktTelefon: ['', Validators.required],
+      drzava: ['', Validators.required],
+      grad: ['', Validators.required],
+      ime: ['', Validators.required],
+      prezime: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      lozinka: ['', [Validators.required, Validators.minLength(8)]],
+      tipKorisnika: ['', Validators.required],
+      jbo: ['', Validators.required],
+      korIme: ['', Validators.required]
+    },  {
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.adminForm.controls; }
+
+  onSubmit() {
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.adminForm.invalid) {
+      return;
+    }
   }
 
 }
